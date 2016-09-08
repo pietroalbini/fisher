25d130118870c181d82507dcf23905624399a9da
Pietro Albini <pietro@pietroalbini.io>
2016-09-08 22:41:15 +0200

    Add an utility to parse an environment file

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -14,6 +14,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 mod tempdir;
+mod parse_env;
 mod copy_to_clone;
 
 #[cfg(test)]
@@ -21,3 +22,4 @@ pub mod testing;
 
 pub use utils::tempdir::create_temp_dir;
 pub use utils::copy_to_clone::CopyToClone;
+pub use utils::parse_env::parse_env;
---
+++ src/utils/parse_env.rs
@@ -0,0 +1,78 @@
+// Copyright (C) 2016 Pietro Albini
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::collections::HashMap;
+
+use errors::{FisherResult, FisherError, ErrorKind};
+
+
+pub fn parse_env(content: &str) -> FisherResult<HashMap<&str, &str>> {
+    let mut result = HashMap::new();
+
+    for line in content.split("\n") {
+        // Skip empty lines
+        if line.trim() == "" {
+            continue;
+        }
+
+        let parts: Vec<&str> = line.splitn(2, "=").take(2).collect();
+        if parts.len() != 2 {
+            return Err(FisherError::new(ErrorKind::InvalidInput(
+                format!("Invalid env received: {}", line)
+            )));
+        }
+
+        let key = parts.get(0).unwrap();
+        let value = parts.get(1).unwrap();
+        result.insert(*key, *value);
+    }
+
+    Ok(result)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use errors::ErrorKind;
+
+    use super::parse_env;
+
+
+    #[test]
+    fn test_parse_env() {
+        // Test with an empty env
+        assert_eq!(parse_env("").unwrap(), HashMap::new());
+
+        // Test with multiple elements
+        let mut expected = HashMap::new();
+        expected.insert("A", "b");
+        expected.insert("B", "c=d=e");
+        assert_eq!(parse_env("A=b\n\nB=c=d=e\n").unwrap(), expected);
+
+        // Test with invalid data
+        let res = parse_env("A=b\nB");
+        assert!(res.is_err());
+        match *res.err().unwrap().kind() {
+            ErrorKind::InvalidInput(..) => {
+                assert!(true)
+            },
+            _ => {
+                panic!("Wrong error received!");
+            },
+        };
+    }
+}
