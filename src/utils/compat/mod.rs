d5cbab13a6825002fbab76c9a2d4640074579316
Pietro Albini <pietro@pietroalbini.org>
2016-12-16 14:53:17 +0100

    Load the recv_timeout polyfill only during tests

--- src/utils/compat/mod.rs
+++ src/utils/compat/mod.rs
@@ -13,6 +13,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
- mod recv_timeout;
-
- pub use self::recv_timeout::*;
+// Currently just used in tests
+#[cfg(test)] mod recv_timeout;
+#[cfg(test)] pub use self::recv_timeout::*;
